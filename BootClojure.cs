6c0dbbfba105163a1482af2367535482d8312fe9
Ramsey Nasser <ram@nas.sr>
2020-07-29 21:13:46 -0400

    Initial commit

---
+++ BootClojure.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using clojure.lang;
+
+public static class BootClojure
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    static void Boot()
+    {
+#if UNITY_IOS
+        RuntimeBootstrapFlag.CodeLoadOrder = new[] { RuntimeBootstrapFlag.CodeSource.InitType };
+        RuntimeBootstrapFlag.DisableFileLoad = true;
+#endif
+    }
+}
